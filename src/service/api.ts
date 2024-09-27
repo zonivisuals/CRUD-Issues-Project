91d94aecc09223f33e004ffa6ecb8b1ca498f8c9
zonivisuals <mohamed.hajji.med@gmail.com>
2024-09-27 15:27:46 +0000

    setting up the local api

---
+++ src/service/api.ts
@@ -0,0 +1,12 @@
+const ISSUES_API = 'http://localhost:4444/api/v1';
+
+//structure of the issue
+interface IssueData {
+    title: string
+    description: string
+    //status: string
+    //type: string 
+}
+
+  
+
