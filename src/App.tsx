73f0cec23673fdd953f743d467bf45afff6b4c16
zonivisuals <mohamed.hajji.med@gmail.com>
2024-09-27 18:01:19 +0000

    importing components and updating App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import Admin from './pages/admin';
+import Sidebar from './components/sidebar';
 import './App.css';
 
 function App() {
   return (
-    <div className='bg-[#0E0F2E] h-screen'>
-      <Navbar/>
-      <Admin/>
+    <div className='bg-[#171652] h-full flex items-start'>
+      <Sidebar/>
+      <div id='content' className='px-8 pt-5 flex-grow h-screen bg-[#0E0F2E] border-none rounded-[1.25rem] m-2'>
+        <Navbar/>
+        <Admin/>
+      </div>
     </div>
   );
 }
