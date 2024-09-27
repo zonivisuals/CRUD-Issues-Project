e6f8a1095abd2829814db9a4d52126f33cfb782d
zonivisuals <mohamed.hajji.med@gmail.com>
2024-09-27 17:57:30 +0000

    implementing 1st admin page ui half

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -3,7 +3,14 @@ import InfoCard from '../components/infoCard'
 
 const admin = () => {
   return (
-    <InfoCard title="All Feedback" totalRequests={1084} monthlyRequests={30} />
+    <div className='py-8'>
+      <div className='flex items-center justify-start gap-5'>
+        <InfoCard title="All Feedback" totalRequests={1084} monthlyRequests={30} />
+        <InfoCard title="Solved" totalRequests={1084} monthlyRequests={30} />
+        <InfoCard title="Pending" totalRequests={1084} monthlyRequests={30} />
+        <InfoCard title="In Progress" totalRequests={1084} monthlyRequests={30} />
+      </div>
+    </div>
   )
 }
 
